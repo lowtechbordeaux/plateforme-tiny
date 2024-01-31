04e0f10de291afc95e112efde922a23c322c3ffc
pacoccino <pac@opac.me>
2024-01-31 13:21:18 +0100

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,66 @@
 export default async function Index() {
   return (
     <div>
-      <div className="flex flex-col items-center py-4 mb-4">
-        <img
-          src="./assets/images/logo-192.png"
-          className="w-24 mb-6"
-          alt="lowtechbordeaux logo"
-        />
+      <div className="flex flex-col items-center py-8">
 
-        <img
-          src="./assets/images/lowtech-title.png"
-          className="w-64 max-w-full mb-2 mx-4"
-          alt="lowtechbordeaux title"
-        />
-        <h1 className="text-md uppercase font-medium font-sans tracking-wide">
-          Bordeaux
-        </h1>
-
-        <p className="font-serif text-center mt-6 mb-2 text-bluey-1 -rotate-6">
-          « Apprendre à faire mieux avec moins »
+        <p className="font-serif text-xl text-center text-bluey-1">
+          Plateforme de mise en relation entre acteurs autour du projet de tiny house low tech
         </p>
+
       </div>
+
       <div className="text-white bg-greeny-1">
         <div className="container py-8">
-          <h2 className="font-serif text-xl font-bold uppercase mb-4">Kézako ?</h2>
+          <h2 className="font-serif text-xl font-bold uppercase mb-4">
+            Kézako ?
+          </h2>
 
           <p>
-            Nous sommes une association qui croit au pouvoir de l’innovation
-            utile, accessible et durable pour répondre aux enjeux d’aujourd’hui
-            et de demain : les low-technologies offrent à chacun et partout, les
-            moyens de répondre à ses besoins dans le respect des Humains et de
-            la Planète !
+            Low Tech Lab Bordeaux va, au cours des deux prochaines années, entreprendre un projet de construction de Tiny House équipées de Low-Techs, tels qu’un poêle de masse, un garde manger, marmite norvégienne ou une jardinière autonome.
+
+            Ce projet fera collaborer différentes associations, et la tiny sera exposée sur les quais de la Garonne à Bordeaux.
           </p>
 
-          <p className="font-serif text-center mt-4 mb-8 -rotate-6">
-            « Son crédo: la Résilience ! »
-          </p>
-
-          <h2 className="text-xl font-bold text-yellowy-1 uppercase mb-4">
-            3 activités principales:
+          <h2 className="text-xl font-bold text-yellowy-1 uppercase my-4">
+            Les associations partenaires:
           </h2>
 
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
             <div className="">
               <h3 className="text-xl font-medium font-serif mb-1">
-                Sensibilisation
+                LowTechLab Bordeaux
               </h3>
-              <ol className="list-decimal list-inside">
-                <li>
-                  Organisation d’actions de sensibilisation auprès du grand
-                  public, scolaires, collectivités & entreprises du territoire.
-                </li>
-                <li>
-                  Animation de la communauté avec des évènements, des
-                  conférences, des sorties, des apéros, etc.
-                </li>
-              </ol>
-            </div>
-            <div className="">
-              <h3 className="text-xl font-medium font-serif mb-1">Makerspace</h3>
-              <ol className="list-decimal list-inside">
-                <li>
-                  Centre de pratique avec outillages professionnels mutualisés,
-                  formations, stages et ateliers.
-                </li>
-                <li>Encadrement d’étudiants de l’enseignement supérieur.</li>
-                <li>
-                  Espace coworking pour les entrepreneurs avec une démarche
-                  low-tech.
-                </li>
-              </ol>
+              <div className="">
+                <p>
+                  Low Tech Lab Bordeaux (LTLB) est une association de Loi 1901 qui croit au pouvoir de l'innovation utile, accessible et durable pour répondre aux enjeux écologiques d'aujourd'hui et de demain.
+
+                  Les basses technologies offrent à chacun et partout, les moyens de répondre à ses besoins dans le respect des Humains et de la Planète.
+
+                </p>
+                <p>
+                  Ses valeurs : Accessibilité, partage, sobriété, résilience, collaboration, autonomie, réemploi
+                </p>
+              </div>
             </div>
             <div className="">
               <h3 className="text-xl font-medium font-serif mb-1">
-                Bureau d’étude
+                Tiny Lab
               </h3>
-              <ol className="list-decimal list-inside">
-                <li>
-                  Mettre la low-tech au service de l’innovation avec son pôle
-                  R&D.
-                </li>
-                <li>
-                  Favoriser l’émergence de solutions low-techs auprès des
-                  collectivités & entreprises.
-                </li>
-                <li>Diffuser en open source avec des outils collaboratifs.</li>
-              </ol>
+              <div>
+                <p>
+                  Association créée par des professionnels de l'éco-construction, dont les activités se concentrent autour de la construction inclusive de tiny houses et habitats légers autonomes.
+                </p>
+              </div>
+            </div>
+            <div className="">
+              <h3 className="text-xl font-medium font-serif mb-1">
+                La planche
+              </h3>
+              <div >
+                <p>
+                  Atelier partagé situé dans le quartier Saint-Michel à Bordeaux entièrement dédié au matériau bois, dans lequel artisans·anes, concepteurs·trices et grand public viennent mutualiser des outils de travail, apprendre, fabriquer et partager leurs savoir-faire.
+                </p>
+              </div>
             </div>
           </div>
         </div>
@@ -127,95 +102,17 @@ export default async function Index() {
       <div className="bg-bluey-1 text-white">
         <div className="container py-8">
           <h2 className="font-serif text-xl font-medium mb-4">
-            Qu’est-ce qu’une low-tech ?
+            Les objectifs
           </h2>
 
           <p>
-            « Une technologie simple répondant à un besoin de base, qui est
-            durable dans le temps, facilement utilisable et constructible, mais
-            est aussi une technologie incluant de la sobriété et participant à
-            votre résilience. »
+            Sensibiliser le grand public autour des enjeux écologiques, l'éco-construction, de la low tech et du réemploi.
+            Proposer des chantiers participatifs où chacun peut venir contribuer ou apprendre à bricoler
+            En créant un maillage des acteurs Low Tech sur le territoire, l’association veut devenir la vitrine du réseau Low Tech de la région, développer des partenariats avec les autres acteurs et créer une dynamique sur le territoire de la Nouvelle-Aquitaine.
           </p>
         </div>
       </div>
 
-      <div>
-        <div className="container py-4">
-          <h2 className="font-serif text-xl font-medium my-4">Nos activités</h2>
-
-          <p>
-            La low-tech nous semble être un outil majeur pour la transition
-            écologique et énergétique, il est donc nécessaire que le plus de
-            monde possible soit informé sur ce que c'est et quelles sont ses
-            applications.
-          </p>
-
-          <p>
-            À cette fin, nous pouvons intervenir pour une large variété de
-            publics, allant des scolaires aux salariés d'entreprises dans des
-            ateliers dédiés, en passant par les stands d'exposition et les
-            conférences ou tables rondes.
-          </p>
-
-          <p>
-            Notre atelier est lui aussi tourné vers la transmission des
-            connaissances et de l'apprentissage de la démarche low-tech. Pour ce
-            faire, nous organisons régulièrement des stages sur une thématique
-            spécifique, et nous mettons petit à petit en place un catalogue de
-            formation couvrant diverses thématiques pour augmenter votre
-            autonomie et faire des économies. C'est aussi un lieu où se
-            retrouvent les adhérents pour co-construire les projets de
-            l'association et participer à ses missions d'intérêt général.
-          </p>
-
-          <p>
-            Les low-techs que nous fabriquons sont conçues, construites et
-            testées par le bureau de Recherche & Développement de l'association,
-            pour être capable de vous fournir des données d'utilisation, de
-            mieux comprendre le fonctionnement des low-techs fabriquées, et
-            adapter les solutions aux besoins spécifiques du territoire. Toutes
-            les low-techs proposées par l'association sont donc réellement
-            utiles, elles participent concrètement à la résilience et
-            l'autonomie des personnes et des structures collectives.
-          </p>
-
-          <h2 className="font-serif text-xl font-medium my-4">Nous rencontrer</h2>
-
-          <p>
-            Tous les <b>mercredis soir</b> (sauf exception), nous organisons des
-            "ateliers" dans nos locaux au Garage Moderne entre membres adhérents,
-            où nous construisons des low-techs.
-            Ce sont des temps conviviaux où vous pourrez découvrir plus
-            amplement l'association avec les bénévoles. N'hésitez pas à nous
-            contacter pour en savoir plus !
-          </p>
-          <p>
-            Vous pouvez aussi directement adhérer et participer dès maintenant
-            aux activités de l'association ! Nous soutenir financièrement ou
-            participer à nos activités permet de nous soutenir. Quelles que
-            soient les compétences et connaissances que vous voulez apporter,
-            vous serez bienvenus , de l'aide ponctuelle au projet de long terme.
-          </p>
-          <p>
-            Low-Tech Bordeaux est uniquement composé de bénévoles.
-          </p>
-
-          <div className="flex justify-center gap-8 mt-8">
-            <a
-              href="https://www.helloasso.com/associations/low-tech-bordeaux#event"
-              target="_blank"
-            >
-              <div className="btn-primary border-yellowy-1">Billetterie</div>
-            </a>
-            <a
-              href="https://www.helloasso.com/associations/low-tech-bordeaux/adhesions/adhesion-ltb"
-              target="_blank"
-            >
-              <div className="btn-primary border-bluey-1">Adhésion</div>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+    </div >
   );
 }
