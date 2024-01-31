5af474fdbb501269c35efd4543a6c95137f30a8b
pacoccino <pac@opac.me>
2024-01-31 13:12:58 +0100

    fix form annonce

--- app/annonce/new/createForm.tsx
+++ app/annonce/new/createForm.tsx
@@ -47,7 +47,6 @@ export default function CreateForm() {
                         inputReq={{
                             required: true,
                             minLength: 3,
-                            maxLength: 20,
                         }}
                         rows={6}
                     />
